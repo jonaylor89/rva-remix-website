49a8e6f1db3c3c391a23425a49ba9931eb0f5be2
Johannes Naylor <jonaylor89@gmail.com>
2022-06-03 23:22:03 -0400

    character

--- src/sections/ScheduleSection.tsx
+++ src/sections/ScheduleSection.tsx
@@ -1,30 +1,37 @@
 
+const character = require('../assets/character.png');
+
 const ScheduleSection = () => {
     return (
         <section id="schedule">
-            <div className="bg-black flex flex-col items-center py-5 lg:mx-40">
-                <div className="font-bold text-4xl pb-5 lg:font-black lg:text-5xl">
-                    SCHEDULE
-                </div>
+            <div className="bg-black flex justify-around py-5 lg:mx-40">
+                <div className="flex flex-col items-start">
+                    <div className="font-bold text-4xl pb-5 lg:font-black lg:text-5xl">
+                        SCHEDULE
+                    </div>
+                    <div className="flex flex-col px-9">
+                        <div className="font-bold text-xl py-3">Saturday (Sept. 17th)</div>
+                        <ul className="flex flex-col list-disc pb-3">
+                            <li>Open Ceremony​</li>
+                            <li>Optional team building</li>
+                            <li>Workshops</li>
+                            <li>Remixing Begins</li>
+                        </ul>
+                        <div className="font-bold text-xl py-3">Sunday-Thursday (Sept. 18th-22nd)</div>
+                        <ul className="flex flex-col list-disc pb-3">
+                            <li>Grind time</li>
+                            <li>Submissions are due Thursday at midnight</li>
+                        </ul>
+                        <div className="font-bold text-xl py-3">Friday (Sept. 23rd)</div>
+                        <ul className="flex flex-col list-disc pb-3">
+                            <li>Official Judging</li>
+                            <li>Community Judging through Social Media</li>
+                        </ul>
+                    </div>
 
-                <div className="flex flex-col px-9">
-                    <div className="font-bold text-xl py-3">Saturday (Sept. 17th)</div>
-                    <ul className="flex flex-col list-disc pb-3">
-                        <li>Open Ceremony​</li>
-                        <li>Optional team building</li>
-                        <li>Workshops</li>
-                        <li>Remixing Begins</li>
-                    </ul>
-                    <div className="font-bold text-xl py-3">Sunday-Thursday (Sept. 18th-22nd)</div>
-                    <ul className="flex flex-col list-disc pb-3">
-                        <li>Grind time</li>
-                        <li>Submissions are due Thursday at midnight</li>
-                    </ul>
-                    <div className="font-bold text-xl py-3">Friday (Sept. 23rd)</div>
-                    <ul className="flex flex-col list-disc pb-3">
-                        <li>Official Judging</li>
-                        <li>Community Judging through Social Media</li>
-                    </ul>
+                </div>
+                <div className="lg:visible">
+                    <img className="w-200 object-contain" src={character} alt="tiny charater" width={400} />
                 </div>
             </div>
         </section>
