a47de792cb706bc570aa75358e6dbb291db39cce
Johannes Naylor <jonaylor89@gmail.com>
2022-08-22 21:41:23 -0400

    prizes

--- src/sections/PrizesSection.tsx
+++ src/sections/PrizesSection.tsx
@@ -1,4 +1,6 @@
 
+const money = require('../assets/money.png')
+
 const PrizesSection = () => {
     return (
         <section id="prizes">
@@ -7,18 +9,23 @@ const PrizesSection = () => {
                     PRIZES
                 </div>
 
+                <img src={money} alt={'money'} width={200}/>
+
                 <div className="flex flex-col lg:flex-row lg:justify-around">
-                    <div className="flex flex-col items-center py-6 lg:px-24">
-                        <div className="font-bold text-xl">1ST PLACE</div>
-                        <div>TBD</div>
+                    <div className="flex flex-col items-center py-6 lg:px-20">
+                        <div className="font-bold text-5xl text-center">1ST PLACE</div>
+                        <div className="text-3xl hover:scale-125 hover:text-green-500">$300</div>
+                        <div className="text-center">Prime slot for the RVA Remix Concert</div>
                     </div>
-                    <div className="flex flex-col items-center py-6 lg:px-24">
-                        <div className="font-bold text-xl">2ND PLACE</div>
-                        <div>TBD</div>
+                    <div className="flex flex-col items-center py-6 lg:px-20">
+                        <div className="font-bold text-5xl text-center">2ND PLACE</div>
+                        <div className="text-3xl hover:scale-125 hover:text-green-500">$200</div>
+                        <div className="text-center">Second slot for the RVA Remix Concert</div>
                     </div>
-                    <div className="flex flex-col items-center py-6 lg:px-24">
-                        <div className="font-bold text-xl">3RD PLACE</div>
-                        <div>TDB</div>
+                    <div className="flex flex-col items-center py-6 lg:px-20">
+                        <div className="font-bold text-5xl text-center">3RD PLACE</div>
+                        <div className="text-3xl hover:scale-125 hover:text-green-500">$100</div>
+                        <div className="text-center">Opening slot for the RVA Remix Concert</div>
                     </div>
                 </div>
             </div>
